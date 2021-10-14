b1bceb1214b142155a6d09aadcb355dc9677ac79
Jonathan Heard <jonathan.l.heard@gmail.com>
2021-10-13 22:29:07 -0700

    Comments removed from earlier version, only thing left to resolve is how to print the % values with a fixed display.

--- yx-jlh1.py
+++ yx-jlh1.py
@@ -8,9 +8,9 @@
 # Tracy couldn't just leave well enough alone... Learning some Python tonight
 
 """ Set initial variables
-    i is the iteration counter
-    index is the location within the MasterList of the start of
-    the LoopList.
+    i = the iteration counter
+    index = the location within the MasterList of the start of
+        the LoopList.
     Creating Odd and Even to keeping track of
     the count of Odd and Even results. """
 i = Odd = Even = 0
@@ -28,43 +28,19 @@ Multiplier = int(input("\n\nEnter a multiplier, Odd not Even. For example 3, 5, 
 
 Num = Max = Seed
 
-""" Note: the numpy module / package may need to be installed for array support
-    In RHEL/CentOS, it is: python3-numpy.x86_64 """
-
-# Create and initialize the MasterList and LoopList arrays
+# Create and initialize the MasterList and LoopList lists
 MasterList = []
 LoopList = []
 
-# Create an infinite loop. The loop will be broken upon a negative number
-# or detected loop.
+# Create an infinite loop. The loop will be broken upon a detected loop.
 while True:
 
-    # Check to see if Num is already in the MasterList array
+    # Check to see that Num is not already in the MasterList array
     if Num not in MasterList:
 
         # Add Num to MasterList
         MasterList.append(int(Num))
 
-        """" # If Num is already in LoopList, the loop is found
-        # break the while loop
-        # if Num in LoopList:
-        #     break
-
-        # Otherwise, add Num to LoopList
-        else:
-            LoopList.append(int(Num))
-
-    # Add Num to MasterList
-    MasterList.append(int(Num)) 
-   # New method of finding the LoopList by jlh
-    try:
-        MasterListIndex = MasterList.index(Num)
-        for LoopListIndex in range(MasterListIndex, len(MasterList)):
-            LoopList.append(MasterList(LoopListIndex))
-            break
-
-    except ValueError:
-        MasterList.append(int(Num)) """
         print("Iteration #", i, ": Num =", int(Num))
 
         i += 1
@@ -74,7 +50,8 @@ while True:
             Even += 1
             Num = Num / 2
 
-        # If Num is odd then apply yx+1 formula it
+        # If Num is odd then apply yx+1 formula, with
+        # y being Num and x being the Multiplier entered.
         else:
             Odd += 1
             Num = Num * Multiplier + 1
@@ -83,10 +60,7 @@ while True:
         if Num > Max:
             Max = Num
 
-    # If Num reaches 1, the loop has been found, break out of the loop
-    # if Num == 1:
-    #if Num in MasterList:
-    # MasterList.append(int(Num))
+    # If Num is in MasterList, a loop has bee found.
     else:
         Index = MasterList.index(Num)
         for LoopListIndex in range(Index, len(MasterList)):
@@ -95,7 +69,7 @@ while True:
 
 """ ##### END OF while LOOP ##### """
 
-print("\nUsing the formula (", int(Multiplier), "* x) + 1")
+print("\nUsing the formula (", int(Multiplier), "* x ) + 1")
 
 print("\nLoop Found!", LoopList)
 
